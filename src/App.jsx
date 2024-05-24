d053882ac9d5c3f65bd045d53a3601c1a34bbc24
SalimovUz <Salimofftolqin7@gmail.com>
2024-05-24 16:17:49 +0500

    Finally

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "./Container/Container";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Container/>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Container/Container.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Calculator from '../Components/Calculator'
+
+const Container = () => {
+  return (
+    <div className='container mx-auto'>
+      <Calculator/>
+    </div>
+  )
+}
+
+export default Container
